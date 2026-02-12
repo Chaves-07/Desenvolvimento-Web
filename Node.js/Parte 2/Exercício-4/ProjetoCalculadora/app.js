8881d7886f6392bdcfb072883c66e079c905047d
Chaves-07 <chavesvinicius320@gmail.com>
2026-02-12 16:17:42 -0300

    #Exercício-4 incompleto

---
+++ Node.js/Parte 2/Exercício-4/ProjetoCalculadora/app.js
@@ -0,0 +1,7 @@
+const calc = require("./utils/operacoes");
+const valid = require("./utils/validacoes");
+
+console.log("Soma:"calc.somar(numA,numB));
+console.log("Subtração:"calc.subtrair(numA,numB));
+console.log("Multiplicação:"calc.multiplicar(numA,numB));
+console.log("Divisão:"calc.dividir(numA,numB));
