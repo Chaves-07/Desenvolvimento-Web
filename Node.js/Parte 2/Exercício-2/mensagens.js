6ec2e9526bbd6695aec0c3ceba0d392a2053da32
Chaves-07 <chavesvinicius320@gmail.com>
2026-02-12 14:12:27 -0300

    #Exercício-2

---
+++ Node.js/Parte 2/Exercício-2/mensagens.js
@@ -0,0 +1,17 @@
+function boasvindas(nome){
+    return nome;
+}
+
+function despedida(nome){
+    return nome;
+}
+
+function autor(nome){
+    return nome;
+}
+
+module.exports = {
+    boasvindas,
+    despedida,
+    autor
+};
---
+++ Node.js/Parte 2/Exercício-2/receba.js
@@ -0,0 +1,5 @@
+const mensagem = require("./mensagens");
+
+console.log(mensagem.boasvindas("oi-Chaves"));
+console.log(mensagem.despedida("tchau-Chaves"));
+console.log(mensagem.autor("Chaves"));
