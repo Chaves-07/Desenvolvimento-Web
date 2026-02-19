819617816bb9f4d3004d1eaac71a6b653ff92277
Chaves-07 <chavesvinicius320@gmail.com>
2026-02-19 14:22:57 -0300

    #Exercício-4

--- Node.js/Parte 2/Exercício-4/ProjetoCalculadora/app.js
+++ Node.js/Parte 2/Exercício-4/ProjetoCalculadora/app.js
@@ -1,7 +1,18 @@
 const calc = require("./utils/operacoes");
 const valid = require("./utils/validacoes");
 
-console.log("Soma:"calc.somar(numA,numB));
-console.log("Subtração:"calc.subtrair(numA,numB));
-console.log("Multiplicação:"calc.multiplicar(numA,numB));
-console.log("Divisão:"calc.dividir(numA,numB));
+const a = "12"
+const b = "33"
+
+if (valid.validarNumero(a) && valid.validarNumero(b)){
+
+    const numA = Number(a);
+    const numB = Number(b);
+
+    console.log("Soma:",calc.somar(numA,numB));
+    console.log("Subtração:",calc.subtrair(numA,numB));
+    console.log("Multiplicação:",calc.multiplicar(numA,numB));
+    console.log("Divisão:",calc.dividir(numA,numB));
+}else{
+    console.log("Valor Inválido");
+}
