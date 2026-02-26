bdc83ea2dec356ad38d95b9f035bc5c2c056c82d
Chaves-07 <chavesvinicius320@gmail.com>
2026-02-26 13:52:45 -0300

    #Node & Express etapa feitas

--- Node & Express/app.js
+++ Node & Express/app.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const app = express();
+const usuarios = [];
 
 app.use(express.json());
-
-const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.send('API funcionando!');
 });
 
+app.get('/usuarios', (req, res) => {
+  res.json(usuarios);
+});
+
+app.post('/usuarios', (req, res) => {
+
+  const usuario = req.body;
+
+  usuarios.push(usuario);
+
+  res.status(201).json(usuario);
+
+});
+
+const PORT = 3000;
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
-
-app.listen(3000);
